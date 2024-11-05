a443cc83005ce04ab0cf35335a91ad4be18ed7af
ismaestro <ramossilvanismael@gmail.com>
2024-11-05 22:32:22 +0100

    feat(lib): fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 rm -Rf dist/
-npm run build
+npm run build:lib
 cp README.md dist/ngx-scroll-to-first-invalid/
 cd dist/ngx-scroll-to-first-invalid
 npm login --registry=https://registry.npmjs.org
-#npm publish --registry=https://registry.npmjs.org --access public
+npm publish --registry=https://registry.npmjs.org --access public
